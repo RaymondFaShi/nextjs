58504b992e45b39e4ac1e8d5425094b2d21845c3
raymond.fa.shi <raymond.fa.shi@gmail.com>
2026-05-09 17:31:06 +0800

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+
+
+import React from "react";
+
 export default function RootPage() {
-    
+
+    React.useEffect( () => {
+
+    }, [] );
+    console.log( window );
 
     return (
         <>This is root</>
