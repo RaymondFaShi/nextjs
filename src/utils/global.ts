62264692c26d83f0557f95b06b205e22d57ea5d0
raymond.fa.shi <raymond.fa.shi@gmail.com>
2026-07-22 08:59:38 +0800

    add

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -13,73 +13,78 @@ const SERVER_STYLE_DEFAULT: string = '\x1b[0m';  // default style,透明色,
 /**
  * 日志实例
  */
-export const log: Log = Object.assign(
-    ( ...args: Array<unknown> ) => {
-        // client
-        if( typeof window !== 'undefined' ) {
-            console.log( "%c[Client Log]", "background: #9e1068;"+ CLIENT_STYLE_DEFAULT, ...args );
-        }
-
-        // server
-        else {
-            console.log( '\x1b[35m'+ '[Server Log]\r\n'+ '\x1b[35m', ...args );
-        }
-    },
-    {
-        i( ...args: Array<unknown> ) {
-            // client
-            if( typeof window !== 'undefined' ) {
-                console.info( "%cINFO", "background: #3c89e8;"+ CLIENT_STYLE_DEFAULT, ...args );
-            }
-
-            // server
-            else {
-                console.info( SERVER_STYLE_INFO, ...args, SERVER_STYLE_DEFAULT );
-            }
-        },
-        s( ...args: Array<unknown> ) {
-            // client
-            if( typeof window !== 'undefined' ) {
-                console.info( "%cSUCCESS", "background: #6abe39;"+ CLIENT_STYLE_DEFAULT, ...args );
-            }
-
-            // server
-            else {
-                console.log( SERVER_STYLE_SUCCESS, ...args, SERVER_STYLE_DEFAULT );
-            }
-        },
-        w( ...args: Array<unknown> ) {
-            // client
-            if( typeof window !== 'undefined' ) {
-                console.warn( ...args );
-            }
-
-            // server
-            else {
-                console.log( SERVER_STYLE_WARNING, ...args, SERVER_STYLE_DEFAULT );
-            }
-        },
-        e( ...args: Array<unknown> ) {
-            // client
-            if( typeof window !== 'undefined' ) {
-                console.error( ...args );
-            }
-
-            // server
-            else {
-                console.log( SERVER_STYLE_ERROR, ...args, SERVER_STYLE_DEFAULT );
-            }
-        },
-        d( ...args: Array<unknown> ) {
-            // client
-            if( typeof window !== 'undefined' ) {
-                console.info( "%cDEBUG", "background: #8c8c8c;"+ CLIENT_STYLE_DEFAULT, ...args );
-            }
-
-            // server
-            else {
-                console.debug( SERVER_STYLE_DEBUG, ...args, SERVER_STYLE_DEFAULT );
-            }
-        }
+const log: Log = ( ...args: Array<unknown> ) => {
+    // client
+    if( typeof window !== 'undefined' ) {
+        console.log( "%c[Client Log]", "background: #9e1068;"+ CLIENT_STYLE_DEFAULT, ...args );
     }
-);
+
+    // server
+    else {
+        console.log( '\x1b[35m'+ '[Server Log]\r\n'+ '\x1b[35m', ...args );
+    }
+}
+
+log.i = ( ...args: Array<unknown> ) => {
+    // client
+    if( typeof window !== 'undefined' ) {
+        console.info( "%cINFO", "background: #3c89e8;"+ CLIENT_STYLE_DEFAULT, ...args );
+    }
+
+    // server
+    else {
+        console.info( SERVER_STYLE_INFO, ...args, SERVER_STYLE_DEFAULT );
+    }
+}
+
+log.s = ( ...args: Array<unknown> ) => {
+    // client
+    if( typeof window !== 'undefined' ) {
+        console.info( "%cSUCCESS", "background: #6abe39;"+ CLIENT_STYLE_DEFAULT, ...args );
+    }
+
+    // server
+    else {
+        console.log( SERVER_STYLE_SUCCESS, ...args, SERVER_STYLE_DEFAULT );
+    }
+}
+
+log.w = ( ...args: Array<unknown> ) => {
+    // client
+    if( typeof window !== 'undefined' ) {
+        console.warn( ...args );
+    }
+
+    // server
+    else {
+        console.log( SERVER_STYLE_WARNING, ...args, SERVER_STYLE_DEFAULT );
+    }
+}
+
+log.e = ( ...args: Array<unknown> ) => {
+    // client
+    if( typeof window !== 'undefined' ) {
+        console.error( ...args );
+    }
+
+    // server
+    else {
+        console.log( SERVER_STYLE_ERROR, ...args, SERVER_STYLE_DEFAULT );
+    }
+}
+
+log.d = ( ...args: Array<unknown> ) => {
+    // client
+    if( typeof window !== 'undefined' ) {
+        console.info( "%cDEBUG", "background: #8c8c8c;"+ CLIENT_STYLE_DEFAULT, ...args );
+    }
+
+    // server
+    else {
+        console.debug( SERVER_STYLE_DEBUG, ...args, SERVER_STYLE_DEFAULT );
+    }
+}
+
+export {
+    log
+}
